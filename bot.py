850e48f7de6be3f3120604de81bc885a66ec8da9
RuneBergh <rune0707@gmail.com>
2018-12-05 18:43:36 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 from pyrogram import Client, Filters
-
+  
 app = Client("silencert")
-
-
 @app.on_message(Filters.group)
+
+
 def delete_join_or_leave_message(client, message):
-    if message.new_chat_members is not None or message.left_chat_member is not None:
-        client.delete_messages(message.chat.id, [message.message_id])
-
-
+	#client.send_message(chat_id, text, disable_notification)
+	if message.new_chat_members is not None or message.left_chat_member is not None:
+       		client.delete_messages(message.chat.id, [message.message_id])
+	if "test" in message.text.lower():
+		client.delete_messages(message.chat.id, [message.message_id])
+	if  message.document:
+		client.delete_messages(message.chat.id, [message.message_id])
 app.start()
 app.idle()
